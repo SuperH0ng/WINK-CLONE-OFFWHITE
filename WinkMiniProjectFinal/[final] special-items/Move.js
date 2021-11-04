f4b175b93ad3ccbda004efb89d26dc08eda96ab4
SuperH0ng <81635179+SuperH0ng@users.noreply.github.com>
2021-11-05 00:40:36 +0900

    WinkMiniProjectFinal(special-items)

---
+++ WinkMiniProjectFinal/[final] special-items/Move.js
@@ -0,0 +1,11 @@
+function moveAccessories() {
+    location.href = "../[final] accessories-combine/accessories-combine.html";
+}
+
+function moveShopMain() {
+    location.href = "../[final] shop-main/shop-main.html";
+}
+
+function moveSpecialItems() {
+    location.href = "../[final] special-items/special-items.html";
+}
